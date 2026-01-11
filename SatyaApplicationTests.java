e38ed51173b9d8c8ff7644a8956617ee1c879147
Satyabrata Behera <satyabrata11x@gmail.com>
2026-01-11 11:54:31 +0530

    Add files via upload

---
+++ SatyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Online_BookStore.Satya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
